43be817971a0fa6993b490985ce8b030d457bf57
Ian S Pringle <ipringle@protonmail.com>
2019-04-14 00:03:06 -0400

    Updated Single Thread Striding, decreased time by dropping use of BufReader

--- find_longest.rs
+++ find_longest.rs
@@ -1,7 +1,5 @@
 use std::env;
-use std::io::BufReader;
-use std::io::prelude::*;
-use std::fs::{self, File};
+use std::fs;
 use std::time;
 
 fn test_for_longest(line: &str, start: usize, 
@@ -17,11 +15,21 @@ fn test_for_longest(line: &str, start: usize,
         if line.chars().nth(x) == Some(' ') {
             //if x is a space and the len of the substring is greater
             //than the prior longest, check if substring is longester
+            //check to the left
+            if x - start > longest_len {
+                longest = test_for_longest(line, start, end - 1,
+                                            longest, longest_len);
+            }
+            //check to the right
             if end - x + 1 > longest_len {
                 longest = test_for_longest(line, start, end - 1,
                                             longest, longest_len);
             }
             //If x is a space, return `longest`
+            //Note explicit `return`
+            //Don't grok the need for this, but it is needed.
+            //irc says it's needed because of the missing `else`, but
+            //the docs don't mention this so ¯\_(ツ)_/¯
             return longest
         }
     }
@@ -32,7 +40,37 @@ fn test_for_longest(line: &str, start: usize,
     //then `collect()` it back into `std::string::String`
     //We use `collect()` because `line` is a `&str` and we want
     //to return a `String` so it needs to be transformed via iteration
+    //I tried the &str[start..end] method but that panicks for $REASONS
+    //A shame because I believe that would speed this up
     line.chars().skip(start).take(end - start + 1).collect()
+}
+
+fn striding_longest(file: String) -> String {
+    let mut longest_len: usize = 0;
+    let mut longest = String::new();
+    for words in file.lines() {
+        let line_len = words.len();
+        if line_len > longest_len {
+            let mut start: usize = 0;
+            let mut pos = longest_len + 1;
+            while pos < line_len {
+                if words.chars().nth(pos) == Some(' ') {
+                    longest = test_for_longest(&words, start, pos - 1,
+                                               longest, longest_len);
+                    longest_len = longest.len();
+                    start = pos + 1;
+                    pos = start + longest_len + 1;
+                } else {
+                    pos = pos + 1;
+                }
+            }
+            if line_len - 1 - start > longest_len {
+                longest = test_for_longest(&words, start, pos - 1,
+                                            longest, longest_len);
+            }
+        }
+    }
+    longest
 }
 
 fn get_longest(words: std::str::SplitWhitespace) -> std::string::String {
@@ -64,11 +102,11 @@ fn main() {
     let results = get_longest(words);
     let elapsed = start.elapsed();
     let seconds = ((elapsed.as_secs() as f64) + (elapsed.subsec_nanos() as f64 / 1_000_000_000.0)) * 1000.0;
-    
+
     println!("Simple:");
     println!("Longest word is '{}' with a length of {} characters.", results, results.len());
     println!("It took {} milliseconds to find this word.", seconds);
-    
+
     //Tried to find an elegant way to get around the below but due to ownership
     //and `std::str::SplitWhitespace` not being well documented to allocate an
     //empty variable of that type, I settled for just reusing the prior `file` and
@@ -99,47 +137,21 @@ fn main() {
     println!("Longest word is '{}' with a length of {} characters.", words[0], words[0].len());
     println!("It took {} milliseconds to find this word.", seconds);
 
-    //See lines 71 through 81 above for why I am re-reallocating `filename`
+    //See lines 71 through 81 above for why I am re-reallocating `filename` and `file`
     let filename = env::args().nth(1).unwrap();
-    //This time, instead of reading the file to a string we will stream it
-    let f = File::open(filename).unwrap();
-    //BufReader is one of three ways of streaming text, it's decidedly the fastest
-    let cursor = BufReader::new(f);
+    let file: String = fs::read_to_string(filename).expect("Bummer, man!");
+    //The above was a `BufReader` but by loading to heap instead of streaming 
+    //this became an entire 1000ms faster. irc said heap would be quicker, guess
+    //they were right.
 
     let start = time::Instant::now();
-    let mut longest_len: usize = 0;
-    let mut longest = String::new();
 
-    //iterate through each line of the cursor
-    for line in cursor.lines() {
-        //`lines` is "wrapped" in a `<Result>`, need to call `unwrap()` to get to string within
-        let words = line.unwrap();
-        let line_len = words.len();
-        let mut start: usize = 0;
-        let mut pos = longest_len + 1;
-        //some lines are shorter than the longest word, no need to even look at them
-        if line_len > longest_len {
-            while pos < line_len {
-                //if the words[x] char is a space...
-                if words.chars().nth(pos) == Some(' ') {
-                    longest = test_for_longest(&words, start, pos - 1,
-                                               longest, longest_len);
-                    longest_len = longest.len();
-                    start = pos + 1;
-                    pos = start + longest_len + 1;
-                } else {
-                    pos = pos + 1;
-                }
-            }
-            if line_len -1 - start > longest_len {
-                longest = test_for_longest(&words, start, pos - 1,
-                                            longest, longest_len);
-            }
-        }
-    }
+    let result = striding_longest(file);
+
     let elapsed = start.elapsed();
     let seconds = ((elapsed.as_secs() as f64) + (elapsed.subsec_nanos() as f64 / 1_000_000_000.0)) * 1000.0;
+
     println!("Striding:");
-    println!("Longest word is '{}' with a length of {} characters.", longest, longest_len);
+    println!("Longest word is '{}' with a length of {} characters.", result, result.len());
     println!("It took {} milliseconds to find this word.", seconds);
 }
